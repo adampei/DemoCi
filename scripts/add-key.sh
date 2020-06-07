db2be5b9596c08feb37f8e62568cb5133ba8a2a0
裴波波 <bobo.pei@xinfenbao.com>
2020-06-07 19:51:31 +0800

    添加缺失证书以及命令

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -5,6 +5,9 @@ security default-keychain -d user -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
+
+security import ./scripts/certs/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+
 echo "导入cer证书"
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 echo "cer证书导入完毕"
@@ -12,6 +15,8 @@ echo "cer证书导入完毕"
 echo "导入p12证书"
 security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P "123456" -A
 echo "p12证书导入完毕"
+
+security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 
 echo "list keychains: "
 security list-keychains
