09615787e447d63233a19ab5f968842e78c30656
裴波波 <bobo.pei@xinfenbao.com>
2020-06-07 17:44:53 +0800

    重新配置证书

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -5,8 +5,13 @@ security default-keychain -d user -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
+echo "导入cer证书"
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+echo "cer证书导入完毕"
+
+echo "导入p12证书"
 security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -A
+echo "p12证书导入完毕"
 
 echo "list keychains: "
 security list-keychains
