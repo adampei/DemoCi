e18e69869e344ba6a939cf34cf3a245fdfdce067
裴波波 <bobo.pei@xinfenbao.com>
2020-06-07 18:43:53 +0800

    修改打包命令

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 echo "开始打包"
-xcrun xcodebuild -project DemoCi.xcodeproj -scheme DemoCi \
-  -archivePath DemoCi.xcarchive archive
+# export_command = 'xcodebuild -exportArchive -archivePath /Users/liangk/Desktop/TestArchive/myArchivePath.xcarchive -exportPath /Users/liangk/Desktop/TestArchive/out -exportOptionsPlist /Users/liangk/Desktop/TestArchive/ExportOptions.plist'
+
+# archive_command = 'xcodebuild archive -workspace %s/%s.xcworkspace -scheme %s -configuration Release -archivePath %s/%s' % (
+
+xcrun xcodebuild archive -project DemoCi.xcodeproj -scheme DemoCi \
+  -archivePath DemoCi.xcarchive
 echo "打包结束"
 
 echo "开始签名"
