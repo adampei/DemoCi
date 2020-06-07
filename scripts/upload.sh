6ba66e0f41d54579f37fc25fa0950616c52e0108
裴波波 <bobo.pei@xinfenbao.com>
2020-06-07 22:30:36 +0800

    更改apikey明文, filepath去掉双引号

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -5,4 +5,4 @@ echo "查看pgyer_upload是否已经下载,并且存在 ls ./scripts "
 ls ./scripts
 echo "查看是否有DemoCi.ipa文件 ls ./scripts/build "
 ls ./scripts/build
-./scripts/pgyer_upload.sh "./scripts/build/$APPNAME.ipa" $PGYER_APIKEY
+./scripts/pgyer_upload.sh ./scripts/build/$APPNAME.ipa "64b07d6a2da9902db04e3b93cd9968da"
