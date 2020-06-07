225ae72a43ce9cd799cccf0729f209791b656552
裴波波 <bobo.pei@xinfenbao.com>
2020-06-07 22:59:19 +0800

    no message

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -6,7 +6,4 @@ ls ./scripts
 echo "查看是否有DemoCi.ipa文件 ls ./scripts/build "
 ls ./scripts/build
 
-path_ipa=$(pwd)/scripts/build
-echo "拼接的路径为: "
-echo "$path_ipa"
-./scripts/pgyer_upload.sh "$path_ipa/$APPNAME.ipa" "64b07d6a2da9902db04e3b93cd9968da"
+./scripts/pgyer_upload.sh "./scripts/build/DemoCi.ipa" "64b07d6a2da9902db04e3b93cd9968da"
