df213f5a1ef004daf45b3cc21be1bf633c25568c
裴波波 <bobo.pei@xinfenbao.com>
2020-06-07 22:19:37 +0800

    添加上传脚本打印

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+echo "开始上传"
+echo "查看pgyer_upload是否已经下载,并且存在 ls ./scripts "
+ls ./scripts
+echo "查看是否有DemoCi.ipa文件 ls ./build "
+ls ./build
 ./scripts/pgyer_upload.sh "./build/$APPNAME.ipa" $PGYER_APIKEY
