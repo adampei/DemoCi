43194760d3d207193db338648af95afc2bfab961
裴波波 <bobo.pei@xinfenbao.com>
2020-06-07 18:30:23 +0800

    添加打印

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+
+echo "开始打包"
 xcrun xcodebuild -project DemoCi.xcodeproj -scheme DemoCi \
   -archivePath DemoCi.xcarchive archive
+echo "打包结束"
 
+echo "开始签名"
 xcrun xcodebuild -exportArchive -archivePath DemoCi.xcarchive \
   -exportPath ./build -exportOptionsPlist ExportOptions.plist
-
+echo "签名结束"
